4547833c123b0fb1f1ccb9fff84be37e7079a7ae
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-01-27 18:18:23 +0100

    sending a message on input

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
+import { useState } from 'react'
 import { io } from 'socket.io-client';
 
 const socket = io('http://localhost:4001', { transports: ['websocket']});
 
-socket.on('connect', () => {
-  console.log('Connected')
-});
-
-socket.on('disconnect', () => {
-  console.log('disconnected')
-})
-
 function App() {
+  socket.on('connect', () => {
+    console.log('Connected')
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+  })
+  
+  const [ msg, setMsg ] = useState('');
+
+  const handleChange = (event) => {
+    setMsg(event.target.value)
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+    if (msg !== '') {
+      socket.emit('message', msg);
+      setMsg('');
+    }  
+  }
+
   return (
     <div>
-      Chat App
+      <form onSubmit={submit}>
+        <input type='text' name='message' value={msg} onChange={handleChange} />
+        <input type='submit' />
+      </form>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-const { Socket } = require('dgram');
-
 const app = require('express')();
 const server = require('http').createServer(app)
 const io = require('socket.io')(server);
@@ -7,7 +5,9 @@ const io = require('socket.io')(server);
 const PORT = process.env.PORT || 4001;
 
 io.on('connection', socket => {
-  console.log(socket.id);
+  socket.on('message', (msg) => {
+    console.log(msg);
+  })
   socket.on('disconnect', () => {
     console.log('disconnected')
   })
