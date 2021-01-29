6e15268a80bc8bd36bc8a13a91e1862cdbe62256
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-01-29 19:21:39 +0100

    login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { io } from 'socket.io-client';
 
+import Form from './components/Form'
+
 const socket = io('http://localhost:4001', { transports: ['websocket']});
 
 function App() {
@@ -11,27 +13,10 @@ function App() {
   socket.on('disconnect', () => {
     console.log('disconnected')
   })
-  
-  const [ msg, setMsg ] = useState('');
-
-  const handleChange = (event) => {
-    setMsg(event.target.value)
-  }
-
-  const submit = (event) => {
-    event.preventDefault();
-    if (msg !== '') {
-      socket.emit('message', msg);
-      setMsg('');
-    }  
-  }
 
   return (
-    <div>
-      <form onSubmit={submit}>
-        <input type='text' name='message' value={msg} onChange={handleChange} />
-        <input type='submit' />
-      </form>
+    <div id="center">
+      <Form />
     </div>
   );
 }
