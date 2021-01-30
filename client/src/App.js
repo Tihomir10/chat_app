fac7ade1df693a6bc76894bfb6fcd2b08caf08cd
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-01-30 16:43:55 +0100

    handle input change, submit username

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,12 @@ import Form from './components/Form'
 const socket = io('http://localhost:4001', { transports: ['websocket']});
 
 function App() {
+  const [ username, setUsername ] = useState();
+
+  const [ inputError, setInputError ] = useState('');
+
+  const [ submit, setSubmit ] = useState(false)
+
   socket.on('connect', () => {
     console.log('Connected')
   });
@@ -14,9 +20,36 @@ function App() {
     console.log('disconnected')
   })
 
+  const handleChange = (event) => {
+    if (event.target.value.length < 4) {
+      setInputError('Username must be 4 characters or longer');
+    } else {
+      setUsername(event.target.value);
+      setInputError('');
+    }
+  }
+
+  const sendUsername = (event) => {
+    if (username) {
+      socket.emit('newUser', username);
+      setSubmit(true)
+    }
+    event.preventDefault();
+  }
+
+  if (submit) {
+    return (
+      <div>Hello {username}</div>
+    )
+  }
+
   return (
     <div id="center">
-      <Form />
+      <Form 
+        handleChange={handleChange}
+        sendUsername={sendUsername}
+        inputError={inputError}
+      />
     </div>
   );
 }
