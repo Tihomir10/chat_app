3f390220f9962f28eacacf3ea68087644d1aab91
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-02-01 21:56:52 +0100

    input name attribute

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,7 +3,7 @@ function Form(props) {
       <form>
         <div className="form-group">
           <label>Username</label>
-          <input type="text" className="form-control" onChange={props.handleChange} />
+          <input type="text" name='username' className="form-control" onChange={props.handleChange} />
           <span>{props.inputError}</span>
         </div>
         <button type="submit" className="btn btn-primary" onClick={props.sendUsername} >Join</button>
