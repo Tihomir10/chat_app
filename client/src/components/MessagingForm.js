0ed22adaf43db072c57079bd6e2480e6b0c2f1cb
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-02-11 19:34:46 +0100

    refactor

--- client/src/components/MessagingForm.js
+++ client/src/components/MessagingForm.js
@@ -1,9 +1,9 @@
 function MessagingForm(props) {
   return (
     <div>
-      <form onSubmit={props.handleSentMessage}>
-        <input name='message' placeholder='Type...' onChange={props.handleChange} />
-        <input type='submit' value='Send' />
+      <form onSubmit={props.handleSentMessage} onClick={props.getReceiverID}>
+        <input  name='message' placeholder='Type...' onChange={props.handleChange} />
+        <input id={props.chatName} type='submit' value='Send' />
       </form>
     </div>
   )
