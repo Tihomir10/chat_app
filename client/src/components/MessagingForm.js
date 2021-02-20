bdc7fc65b981b0b139243147f96ef6aa4e9e6e38
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-02-20 20:54:59 +0100

    added classes to elements, onclick func to input

--- client/src/components/MessagingForm.js
+++ client/src/components/MessagingForm.js
@@ -1,10 +1,10 @@
 function MessagingForm(props) {
   return (
     <div>
-      <form name={props.chatName} onSubmit={props.handleSentMessage} onClick={props.getReceiverID}>
-        <input  name='message' placeholder='Type...' onChange={props.handleChange} />
-        <input id={props.receiverID} type='submit' value='Send' />
-      </form>
+      <form className='messaging-form' onSubmit={props.handleSentMessage}>
+        <input id='message' name='message' className={props.chatName} onClick={props.updateMessageForSending} onChange={props.handleChange} />
+        <input className={props.chatName} type='submit' value='Send' />
+    </form>
     </div>
   )
 }
