d4790da89757051f20aac5b1053fcc0cf6ce5794
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-04 11:02:00 +0100

    login form

--- client/src/features/user/LoginForm.js
+++ client/src/features/user/LoginForm.js
@@ -1,14 +1,48 @@
-function LoginForm(props) {
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { userAdded } from './usersSlice'
+
+export const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dipatch = useDispatch();
+
+  const onUsernameChanged = e => setUsername(e.target.value)
+  const onPasswordChanged = e => setPassword(e.target.value)
+
+  const onLoginButtonClicked = () => {
+    if (username && password) {
+      dipatch(userAdded(username, password))
+      setUsername('')
+      setPassword('')
+    }
+  }
+
+  const canLogin = Boolean(username) && Boolean(password)
+
   return (
     <form className='center'>
       <div className="form-group">
-        <label>Username</label>
-        <input type="text" name='username' className="form-control" onChange={props.handleChange} />
-        <span>{props.inputError}</span>
+        <label htmlFor='username'>Username:</label>
+        <input 
+          type='text' 
+          name='username' 
+          className="form-control" 
+          value={username}
+          onChange={onUsernameChanged}
+        />
+        <label htmlFor='password'>Password:</label>
+        <input 
+          type='password'
+          name='password'
+          className="form-control"
+          value={password}
+          onChange={onPasswordChanged}
+        />
       </div>
-      <button type="submit" className="btn btn-primary" onClick={props.sendUsername} >Join</button>
+      <button type="button"  className="btn btn-primary" onClick={onLoginButtonClicked} disabled={!canLogin} >Login</button>
     </form>
   )
 }
-
-export default LoginForm;
