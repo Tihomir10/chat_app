fa3a6729c0409a59c1366a71caa47b4593835c94
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-06 15:31:13 +0100

    post user data

--- client/src/features/user/usersSlice.js
+++ client/src/features/user/usersSlice.js
@@ -1,6 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = []
+
+export const fetchUser = createAsyncThunk('user/fetchUser', async (data) => {
+  const response = await axios.post('http://localhost:4001/api/register', data)
+  console.log('im fetching', response)
+})
 
 const usersSlice = createSlice({
   name: 'users',
