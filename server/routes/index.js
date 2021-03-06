6f98440bdee3b41b314d23b32f7118a5cc7836fa
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-06 15:29:05 +0100

    first commit

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var router = express.Router()
+
+router.post('/register', (req, res) => {
+  console.log(req.body)
+})
+
+router.post('/login', (req, res) => {
+  console.log(req.body)
+})
+
+module.exports = router
