1ed5b01471612eee9a251c05f2bb8e905053e024
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-08 10:55:44 +0100

    handling respone after user registration

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -5,9 +5,13 @@ axios.defaults.baseURL = 'http://localhost:4001'
 export const post = async (resource, data = {}) => {
   const response = await axios.post(resource, data)
 
-  if (response.status === 200) {
-    return response
-  } else {
-    return Promise.reject(response.status)
-  }
+  if (response.status === 200 && response.data.code === 201) {
+    console.log(response)
+    return response.data
+  } else if (response.status === 200 && response.data.code === 409) {
+    console.log(response)
+    return response.data.code
+  } 
 }
+
+ 
