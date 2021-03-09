b8e2635fbdb7aa6c25890d9f2f2171d24aa5797c
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-09 15:24:33 +0100

    extraReducers for user login, renamed functions

--- client/src/features/user/usersSlice.js
+++ client/src/features/user/usersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 import { history } from '../..//history'
-import { registerUser } from '../../api'
+import { callRegisterUser, callLoginUser } from '../../api'
 
 const initialState = {
   currentUser: [],
@@ -9,8 +9,13 @@ const initialState = {
   errorMsg: null
 }
 
-export const postUser = createAsyncThunk('user/fetchUser', async (data) => {
-  const response = await registerUser(data)
+export const registerUser = createAsyncThunk('user/registerUser', async (data) => {
+  const response = await callRegisterUser(data)
+  return response
+})
+
+export const loginUser = createAsyncThunk('user/loginUser', async (data) => {
+  const response = await callLoginUser(data)
   return response
 })
 
@@ -19,11 +24,21 @@ const usersSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers : {
-  [postUser.rejected]: (state, action) => {
+  [registerUser.rejected]: (state, action) => {
     state.errorMsg = action.payload;
     state.status = "error"
   },
-  [postUser.fulfilled]: (state, action) => {
+  [registerUser.fulfilled]: (state, action) => {
+    state.status = 'success'
+    const { name, userId } = action.payload
+    state.currentUser.push(name, userId)
+    history.push(action.payload.redirectUrl)
+  },
+  [loginUser.rejected]: (state, action) => {
+    state.errorMsg = action.payload
+    state.status = 'error'
+  },
+  [loginUser.fulfilled]: (state, action) => {
     state.status = 'success'
     const { name, userId } = action.payload
     state.currentUser.push(name, userId)
