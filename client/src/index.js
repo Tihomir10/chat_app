5cc6d4d837a9d404dda661afce4c80a05a0bbe0e
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-09 14:38:25 +0100

    packages, refactor, deletion

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <React.StrictMode store={store}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <React.StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
