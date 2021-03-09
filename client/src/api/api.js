f5ec4dbf396e1ca45106a900ab9bf045c1af15da
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-09 14:32:02 +0100

    deleted logs, expanded response handling

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -6,12 +6,12 @@ export const post = async (resource, data = {}) => {
   const response = await axios.post(resource, data)
 
   if (response.status === 200 && response.data.code === 201) {
-    console.log(response)
     return response.data
   } else if (response.status === 200 && response.data.code === 409) {
-    console.log(response)
     return response.data.code
-  } 
+  } else {
+    return response.data.error
+  }
 }
 
  
