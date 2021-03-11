cb907d3d30eb82c5d5cf3fdef8f37132a26f3e31
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-11 13:15:30 +0100

    show list of users

--- client/src/features/listOfUsers/ListOfUsers.js
+++ client/src/features/listOfUsers/ListOfUsers.js
@@ -1,7 +1,18 @@
-export const ListOfUsers= () => {
+import { Link } from 'react-router-dom'
+
+export const ListOfUsers = ({ users }) => {  
+  const renderedUsers = users.map(user => {
+    return (
+      <li key={user.id}>
+        <Link to={`/chat/${user.id}`}>{user.name}</Link>
+      </li>
+    )
+  })
+
   return (
     <ul>
-      <li>User</li>
+      <h4>Users online:</h4>
+      {renderedUsers}
     </ul>
   )
 }
