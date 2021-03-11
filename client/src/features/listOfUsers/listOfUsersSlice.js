81326ff1020486346c7c0b29f097a057cbcf7d31
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-11 10:00:44 +0100

    defined a slice, fetch users

---
+++ client/src/features/listOfUsers/listOfUsersSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { callGetUsers } from '../../api'
+
+const initialState = {
+  listOfUsers: [],
+  status: 'idle',
+  errorMsg: null
+}
+
+export const fetchUsers = createAsyncThunk('listOfUsers', async () => {
+  const response = await callGetUsers()
+  return response
+})
+
+const listOfUsersSlice = createSlice({
+  name: 'listOfUsers',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchUsers.rejected]: (state, action) => {
+      state.status = 'error'
+      state.errorMsg = action.payload
+    },
+    [fetchUsers.fulfilled]: (state, action) => {
+      state.status = 'success'
+      console.log(action.payload)
+      state.listOfUsers = (action.payload)
+    }
+  }
+})
+
+export default listOfUsersSlice.reducer
