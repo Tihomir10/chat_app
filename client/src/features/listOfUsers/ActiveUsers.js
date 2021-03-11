8e5e4c1aa7c61cd21d4dc463005804bb3fb45592
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-11 10:07:58 +0100

    dispatch fetchUsers when component renders

--- client/src/features/listOfUsers/ActiveUsers.js
+++ client/src/features/listOfUsers/ActiveUsers.js
@@ -1,7 +1,17 @@
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+
+import { fetchUsers } from './listOfUsersSlice'
+
 import { Heading } from './Heading'
 import { ListOfUsers } from './ListOfUsers';
 
 export const ActiveUsers = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchUsers())
+  })
+  
   return (
     <div className='users'>
       <Heading />
