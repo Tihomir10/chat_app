ced9e7c4f510a8b2a96e310686623dd35219d32f
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-12 11:23:12 +0100

    refactor

--- client/src/features/chat/ChatForm.js
+++ client/src/features/chat/ChatForm.js
@@ -1,12 +1,8 @@
-function ChatForm(props) {
+export const ChatForm = () => {
   return (
-    <div>
-      <form className='messaging-form' onSubmit={props.handleSentMessage}>
-        <input id='message' name='message' value={props.text} className={props.chatName} onClick={props.updateMessageForSending} onChange={props.handleChange} />
-        <input className={props.chatName} type='submit' value='Send' />
+    <form className='messaging-form'>
+      <input id='message' name='message'/>
+      <input id='submit-msg' type='submit' value='Send' />
     </form>
-    </div>
   )
 }
-
-export default ChatForm;
