2c4efa434ddbca3a14ef301c324f4c708605e9d2
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-13 09:20:47 +0100

    get id and find user

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -1,12 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+
+import { selectUserById } from '../listOfUsers/listOfUsersSlice'
+import { selectedChatBuddy } from './chatSlice'
 
 import { ChatForm } from './ChatForm'
 import { ChatHistory } from './ChatHistory'
 
-export const Chat = () => {
+export const Chat = ({ match }) => {
+  const dispatch = useDispatch()
+  const { id } = match.params
+  
+  const chatBuddy = useSelector(state => selectUserById(state, id))
+
+  if (chatBuddy) {
+    dispatch(selectedChatBuddy({chatBuddy}))
+  }
+
   return (
     <div className='chats'>
       <Link to='/chat'>Back to Chat</Link>
+      <h4>Talking to {chatBuddy.name}</h4>
       <ChatForm />
       <ChatHistory />      
     </div>
