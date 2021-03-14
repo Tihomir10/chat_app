e233c1f68fbea1d7d00e809b010c70236e0cb420
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-14 10:57:29 +0100

    hook and onchange function

--- client/src/features/chat/ChatForm.js
+++ client/src/features/chat/ChatForm.js
@@ -1,8 +1,14 @@
+import React, { useState } from 'react'
+
 export const ChatForm = () => {
+  const [message, setMessage] = useState('')
+
+  const onMessageChanged = e => setMessage(e.target.value)
+
   return (
-    <form className='messaging-form'>
-      <input id='message' name='message'/>
-      <input id='submit-msg' type='submit' value='Send' />
+    <form className='messaging-form' onSubmit={onSubmitButtonClicked}>
+      <input id='message' name='message' value={message} onChange={onMessageChanged}/>
+      <input id='submit-msg' type='submit' value='Send'/>
     </form>
   )
 }
