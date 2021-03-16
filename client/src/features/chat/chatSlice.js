296a7308eefc240bfd2c345b8521ca8703e71392
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-16 10:26:29 +0100

    emit a private message

--- client/src/features/chat/chatSlice.js
+++ client/src/features/chat/chatSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import socket from '../../socket'
 
 const initialState = {
   chats: [],
@@ -14,10 +15,14 @@ const chatSlice = createSlice({
     selectedChatBuddy(state, action) {
       const { chatBuddy } = action.payload
       state.chatBuddy = chatBuddy
+    },
+    sendMessage(state, action) {
+      const { id, message } = action.payload
+      socket.emit('private message', {id, message})
     }
   }
 })
 
-export const { selectedChatBuddy } = chatSlice.actions
+export const { selectedChatBuddy, sendMessage } = chatSlice.actions
 
 export default chatSlice.reducer
