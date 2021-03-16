e22dfdb5c678c0ff08ec341927b7169b45982a69
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-16 10:26:49 +0100

    first commit

---
+++ server/socket.js
@@ -0,0 +1,41 @@
+const socketConnection = (io) => {
+  io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    if (!username) {
+      return next(new Error("invalid username"));
+    }
+    socket.username = username;
+    next();
+  });
+
+  const findUsers = (users) => {
+    for (let [id, socket] of io.of('/').sockets) {
+      users.push({
+        id: id,
+        name: socket.username
+      })
+    }
+    return users
+  }
+  
+  io.on('connection', (socket) => {
+    const users = []
+    findUsers(users)
+    io.emit('users', users)
+
+    socket.on('disconnect', () => {
+      const users = []
+      findUsers(users)
+      io.emit('users', users)
+    })
+
+    socket.on('private message', ({ id, message }) => {
+      socket.to(id).emit('private message', {
+        from: socket.id,
+        message
+      })
+    })
+  })
+}
+
+module.exports = socketConnection
