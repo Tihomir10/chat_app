41b010890c216c0c03ad7f05e880bd831bdbad1c
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-17 12:42:56 +0100

    refactor, selector functions, save incoming messages

--- client/src/features/chat/chatSlice.js
+++ client/src/features/chat/chatSlice.js
@@ -4,6 +4,7 @@ import socket from '../../socket'
 const initialState = {
   chats: [],
   chatBuddy: {},
+  listOfUsers: [],
   status: 'idle',
   errorMsg: null
 }
@@ -13,16 +14,35 @@ const chatSlice = createSlice({
   initialState,
   reducers: {
     selectedChatBuddy(state, action) {
-      const { chatBuddy } = action.payload
-      state.chatBuddy = chatBuddy
+      state.chatBuddy = action.payload
     },
     sendMessage(state, action) {
       const { id, message } = action.payload
       socket.emit('private message', {id, message})
+    },
+    receiveMessage(state, action) {
+      for(var i = 0; i < state.chats.length; i++) {
+        if (state.chats[i].senderName == action.payload.senderName) {
+          state.chats[i].messages = state.chats[i].messages.concat(action.payload.messages[0])
+          return;
+        }
+      }
+      state.chats.push(action.payload)
+    },
+    setUsers(state, action) {
+      state.listOfUsers = action.payload
     }
   }
 })
 
-export const { selectedChatBuddy, sendMessage } = chatSlice.actions
+export const { selectedChatBuddy, sendMessage, receiveMessage, setUsers } = chatSlice.actions
+
+export const selectListOfUsers = state => state.chat.listOfUsers
+
+export const selectUserById = (state, id) => 
+  state.chat.listOfUsers.find(user => user.id === id)
+
+export const selectChatById = (state, id) =>
+  state.chat.chats.find(chat => chat.from === id)
 
 export default chatSlice.reducer
