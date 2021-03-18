70ad36fdb1cf8ca85d1a63339b8875897ba34296
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-18 13:03:40 +0100

    selector functions, generate unique name for chat object, handle socket events

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -1,27 +1,50 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
 
-import { selectUserById } from '../listOfUsers/listOfUsersSlice'
-import { selectedChatBuddy } from './chatSlice'
+import { selectedChatBuddy, receiveMessage, selectUserById, setUsers, selectListOfUsers, selectChatById } from './chatSlice'
+import socket from '../../socket'
 
 import { ChatForm } from './ChatForm'
 import { ChatHistory } from './ChatHistory'
+import { ActiveUsers } from './ActiveUsers'
 
 export const Chat = ({ match }) => {
   const dispatch = useDispatch()
+  const history = useHistory()
   const { id } = match.params
 
+  const users = useSelector(selectListOfUsers) 
   const chatBuddy = useSelector(state => selectUserById(state, id))
 
+  
+  const currentUser = useSelector(state => state.user.currentUser)
+  let chatName
+
   if (chatBuddy) {
-    dispatch(selectedChatBuddy({chatBuddy}))
+    dispatch(selectedChatBuddy(chatBuddy))
+    chatName = (currentUser.name + chatBuddy.name).split('').sort().join('')
+  }
+
+  useEffect(() => {
+    socket.on('users', users => {
+      dispatch(setUsers(users))
+    })
+
+    socket.on('private message', content => {
+      dispatch(receiveMessage(content))
+    })
+  }, [])
+
+    
+  if (id && !chatBuddy) {
+    history.push('/')
   }
 
   if (!chatBuddy) {
     return (
-      <div className='chats'>
-        <Link to='/chat'>Back to Chat</Link>
-        <h4>User not found</h4>
+      <div className='users'>
+        <ActiveUsers users={users} />
       </div>
     )
   }
@@ -30,8 +53,8 @@ export const Chat = ({ match }) => {
     <div className='chats'>
       <Link to='/chat'>Back to Chat</Link>
       <h4>Talking to {chatBuddy.name}</h4>
-      <ChatForm id={id} />
-      <ChatHistory />      
+      <ChatForm id={id} chatName={chatName} senderName={currentUser.name} />
+      <ChatHistory id={id} chatName={chatName} />      
     </div>
   )
 }
