947536c280e472ac98b5d8f40ba5bdccc1600ab4
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-03-19 19:09:35 +0100

    removed listofusers

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from '../features/user/currentUserSlice'
-import listOfUsersReducer from '../features/listOfUsers/listOfUsersSlice'
 import chatReducer from '../features/chat/chatSlice'
 
 export default configureStore({
   reducer: {
     user: userReducer,
-    listOfUsers: listOfUsersReducer,
     chat: chatReducer
   }
 })
